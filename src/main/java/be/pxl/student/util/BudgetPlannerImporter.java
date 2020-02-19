e90517581fadc32e819bf32bb849496c3f81814c
Mariel Latella Barros <mariel.latella@gmail.com>
2020-02-19 15:08:51 +0100

    add skeleton

--- src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
+++ src/main/java/be/pxl/student/util/BudgetPlannerImporter.java
@@ -4,4 +4,17 @@ package be.pxl.student.util;
  * Util class to import csv file
  */
 public class BudgetPlannerImporter {
+
+    //TODO: Create constructor with path to file
+
+    //TODO: Read lines from file.
+        //TODO: Per line create an Account and a Payment object
+        //TODO: Add to list
+
+    //TODO: Use log4j to print the Account toString to console
+
+    //TODO: Use log4j to print the Account toString to an output.log file
+
+    //TODO: Use log4j to print errors and warnings
+
 }
