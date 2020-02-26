44d419de38835c904077d7239d07d0badb5bc98f
Mariel Latella Barros <mariel.latella@gmail.com>
2020-02-26 14:25:05 +0100

    add tests for payment currency, detail

--- src/main/test/be/pxl/student/util/BudgetPlannerImporterTest.java
+++ src/main/test/be/pxl/student/util/BudgetPlannerImporterTest.java
@@ -21,5 +21,34 @@ public class BudgetPlannerImporterTest {
         Assertions.assertEquals(payment.getAmount(), (payment.getAmount() * 100) / 100);
     }
 
-    
+    @Test
+    public void paymentShouldReturnCorrectDetail(){
+        String line = "Jos,BE69771770897312,BE15563218722574,Sun Feb 02 16:49:24 CET 2020,-2263.59,EUR,Veritatis qui voluptas nostrum iste vitae maxime enim.";
+        BudgetPlannerImporter bpi = new BudgetPlannerImporter();
+        Payment payment = bpi.getPayment(line);
+
+        Payment payment2 = new Payment();
+        payment2.setAmount(-2263.59f);
+        payment2.setCurrency("EUR");
+        payment2.setDetail("Veritatis qui voluptas nostrum iste vitae maxime enim.");
+
+        Assertions.assertEquals("EUR", payment.getCurrency());
+        Assertions.assertEquals(payment.getDetail(), payment2.getDetail());
+    }
+
+    @Test
+    public void paymentShouldReturnCorrectCurrency(){
+        String line = "Jos,BE69771770897312,BE15563218722574,Sun Feb 02 16:49:24 CET 2020,-2263.59,EUR,Veritatis qui voluptas nostrum iste vitae maxime enim.";
+        BudgetPlannerImporter bpi = new BudgetPlannerImporter();
+        Payment payment = bpi.getPayment(line);
+
+        Payment payment2 = new Payment();
+        payment2.setAmount(-2263.59f);
+        payment2.setCurrency("EUR");
+        payment2.setDetail("Veritatis qui voluptas nostrum iste vitae maxime enim.");
+
+        Assertions.assertEquals("EUR", payment.getCurrency());
+        Assertions.assertEquals(payment.getCurrency(), payment2.getCurrency());
+    }
+
 }
