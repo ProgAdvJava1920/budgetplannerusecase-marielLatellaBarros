697b113e76dfaddf0708ee0e81e6c3316f10541d
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-05-13 23:12:31 +0200

    Refactor method so an account can be found either by name OR iban. Implement save account to database and update for account found based in iban.

--- src/main/java/Controllers/AccountController.java
+++ src/main/java/Controllers/AccountController.java
@@ -20,46 +20,72 @@ public class AccountController {
     private static final Logger LOGGER = LogManager.getLogger(AccountController.class);
 
     @GET
-    public String sayHello(){
+    public String sayHello() {
         return "Hi, Mariel";
     }
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response createAccount(AccountDTO accountDTO) {
         EntityManager em = EntityManagerUtil.createEntityManager();
 
-            try {
-                AccountDAO accountDAO = new AccountDAOImpl(em);
-                Account accountFoundByName = accountDAO.getByName(accountDTO.getName());
-                Account accountFoundByIban = accountDAO.getByIban(accountDTO.getIBAN());
+        try {
+            AccountDAO accountDAO = new AccountDAOImpl(em);
+            Account accountFoundInDatabase = accountDAO.getByNameOrIban(mapAccountDTO(accountDTO));
 
-                if (accountFoundByIban.getIban().equalsIgnoreCase(accountDTO.getIBAN()) &&
-                        (accountFoundByIban.getName() == null) || accountFoundByIban.getName().equalsIgnoreCase("")) {
-                    LOGGER.warn("Fill in the account holder name for this iban {}", accountDTO.getIBAN());
-                    ErrorAccountResponse er = new ErrorAccountResponse("Fill in the account holder name for this iban: " +
-                            accountDTO.getIBAN());
-                    return Response.status(Response.Status.BAD_REQUEST).entity(er).build();
+            //Account not found in database. Create new account
+            if (accountFoundInDatabase == null) {
+                Account newAccount = accountDAO.saveAccount(mapAccountDTO(accountDTO));
+                LOGGER.info("Account created successfully: {}", newAccount.getId());
+                return Response.created(UriBuilder.fromPath("/api/accounts/" + newAccount.getId()).build()).build(); //201
 
-                } else if (accountFoundByName != null) {
-                    LOGGER.warn("There already exists an account with name: {}", accountDTO.getName());
-                    ErrorAccountResponse er = new ErrorAccountResponse("There already exists an account with name " + accountDTO.getName());
+                //Account found in database
+            } else {
+                //Avoid duplicates
+                if (accountNameAndIbanAlreadyExist(accountDTO, accountFoundInDatabase)) {
+                    LOGGER.warn("Avoid duplicate for name: {}, iban: {}", accountDTO.getName(), accountDTO.getIBAN());
+                    ErrorAccountResponse er = new ErrorAccountResponse(
+                            "There already exists an account with name " + accountDTO.getName() +
+                                    " and iban " + accountDTO.getIBAN());
                     return Response.status(Response.Status.BAD_REQUEST).entity(er).build();
-                } else {
-                    Account newAccount = accountDAO.saveAccount(mapAccountDTO(accountDTO));
-                    if (newAccount != null) {
-                        LOGGER.info("Account created successfully: {}", newAccount.getId());
-                        return Response.created(UriBuilder.fromPath("/api/accounts/" + newAccount.getId()).build()).build(); //201
+                    //update account name based on iban
+                    //account name does not exist in database
+                } else if (accountNameIsNullOrEmpty(accountFoundInDatabase)) {
+                    boolean isUpdated = accountDAO.updateAccount(mapAccountDTO(accountDTO));
+                    if (isUpdated) {
+                        return Response.status(Response.Status.NO_CONTENT).entity("").build();
                     } else {
-                        LOGGER.warn("Something went wrong and no account was created");
-                        ErrorAccountResponse er = new ErrorAccountResponse("Something went wrong!");
+                        LOGGER.warn("Something went wrong and no account was updated");
+                        ErrorAccountResponse er = new ErrorAccountResponse("Something went and no account was updated!");
                         return Response.status(Response.Status.BAD_REQUEST).entity(er).build();
                     }
-                }
-            } finally {
-                if (em != null) {
-                    em.close();
+                    //account name exists in database but with another iban
+                } else {
+                    LOGGER.warn("There already exists an account with iban: {} with another name: {}",
+                            accountDTO.getIBAN(), accountDTO.getName());
+                    ErrorAccountResponse er = new ErrorAccountResponse("There already exists an account with iban " +
+                            accountDTO.getIBAN() + " for a different account.");
+                    return Response.status(Response.Status.BAD_REQUEST).entity(er).build();
                 }
             }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    private boolean accountNameIsNullOrEmpty(Account accountFoundInDatabase) {
+        return accountFoundInDatabase.getName().isBlank() ||
+                accountFoundInDatabase.getName().isEmpty() ||
+                accountFoundInDatabase.getName() == null;
+    }
+
+    private boolean accountNameAndIbanAlreadyExist(AccountDTO accountDTO, Account accountFoundInDatabase) {
+        if (accountFoundInDatabase.getName().equals(accountDTO.getName())) {
+            return accountFoundInDatabase.getIban().equals(accountDTO.getIBAN());
+        }
+        return false;
     }
 
     private Account mapAccountDTO(AccountDTO accountDTO) {
