82c3a4a5e0a13cee6d74fd4eb4063966610351b3
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-06-12 13:35:07 +0200

    Refactor interface as all implementations will make use of this interface to get access to the main CRUD actions

---
+++ src/main/java/Repositories/DAO.java
@@ -0,0 +1,13 @@
+package Repositories;
+
+import java.util.List;
+
+public interface DAO<T, E extends Exception> {
+    T create(T t) throws E;
+    T getById(int id) throws E;
+    T getByNameOrIban(T t) throws E;
+    List<T> getAll() throws E;
+    T getByIban(String iban) throws E;
+    boolean update(T t) throws E; //T update (T t)
+    T delete(T t) throws E;
+}
