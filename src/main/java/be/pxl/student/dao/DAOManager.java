301996c6220dc819feb444f6b852a6d75ffb04a5
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-06-12 14:24:54 +0200

    Get a connection from the jdbc Driver manager

--- src/main/java/be/pxl/student/dao/DAOManager.java
+++ src/main/java/be/pxl/student/dao/DAOManager.java
@@ -4,9 +4,35 @@ package be.pxl.student.dao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DAOManager {
     private static Logger logger = (Logger) LogManager.getLogger(DAOManager.class);
 
-    
+    private String url;
+    private String username;
+    private String password;
+    private Connection connection;
+
+    public DAOManager(String url) {
+        this.url = url;
+    }
+
+    public DAOManager(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (connection == null || (connection.isClosed())) {
+            connection = DriverManager.getConnection(url);
+            connection.setAutoCommit(false);
+        }
+        return connection;
+    }
+
 
 }
