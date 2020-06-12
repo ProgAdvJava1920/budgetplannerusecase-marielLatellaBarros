abcb0249169cd33281b83957b034e7d0c581cd5b
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-06-12 14:03:09 +0200

    Add logger to track changes, errors, info

---
+++ src/main/java/be/pxl/student/dao/DAOManager.java
@@ -0,0 +1,12 @@
+package be.pxl.student.dao;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+public class DAOManager {
+    private static Logger logger = (Logger) LogManager.getLogger(DAOManager.class);
+
+    
+
+}
