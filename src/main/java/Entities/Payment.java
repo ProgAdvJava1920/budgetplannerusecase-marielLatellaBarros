7cb71a29fd6504ad87457c31bf86b37307293768
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-06-12 16:23:41 +0200

    Add missing accountId and counterAccountId

--- src/main/java/Entities/Payment.java
+++ src/main/java/Entities/Payment.java
@@ -1,8 +1,5 @@
 package Entities;
 
-import Entities.Account;
-import Entities.Label;
-
 import javax.persistence.*;
 import java.util.Date;
 
@@ -12,10 +9,12 @@ import java.util.Date;
                 query = "SELECT p FROM Payment p"),
 })
 public class Payment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int Id;
+    private int Id; //FK
+    private int accountId;//FK
+    private int counterAccountId;//FK
+    private String IBAN;
     @Temporal(TemporalType.TIMESTAMP)
     private Date date;
     private float amount;
@@ -23,23 +22,24 @@ public class Payment {
     private String currency;
     private String detail;
 
-    @JoinColumn(name = "accountId")
-    @ManyToOne
-    //@Column(nullable = false)
-    private Account account;
-
-    @JoinColumn(name = "counterAccountId")
-    @ManyToOne
-    //@Column(nullable = false)
-    private Account counterAccount;
-
-    @JoinColumn(name = "labelId")
-    @ManyToOne
-    private Label label;
+//    @JoinColumn(name = "accountId")
+//    @ManyToOne
+//    //@Column(nullable = false)
+//    private Account account;
+//
+//    @JoinColumn(name = "counterAccountId")
+//    @ManyToOne
+//    //@Column(nullable = false)
+//    private Account counterAccount;
+//
+//    @JoinColumn(name = "labelId")
+//    @ManyToOne
+//    private Label label;
 
     public Payment() { }
 
-    public Payment(Date date, float amount, String currency, String detail) {
+    public Payment(String IBAN, Date date, float amount, String currency, String detail) {
+        this.IBAN = IBAN;
         this.date = date;
         this.amount = amount;
         this.currency = currency;
@@ -47,6 +47,30 @@ public class Payment {
     }
 
     public int getId() { return Id; }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getCounterAccountId() {
+        return counterAccountId;
+    }
+
+    public void setCounterAccountId(int counterAccountId) {
+        this.counterAccountId = counterAccountId;
+    }
+
+    public String getIBAN() {
+        return IBAN;
+    }
+
+    public void setIBAN(String IBAN) {
+        this.IBAN = IBAN;
+    }
 
     public Date getDate() {
         return date;
@@ -76,11 +100,11 @@ public class Payment {
         this.detail = detail;
     }
 
-    public Account getAccount() { return account; }
-    public void setAccount(Account account) { this.account = account; }
-
-    public Account getCounterAccount() { return counterAccount; }
-    public void setCounterAccount(Account counterAccount) { this.counterAccount = counterAccount; }
+//    public Account getAccount() { return account; }
+//    public void setAccount(Account account) { this.account = account; }
+//
+//    public Account getCounterAccount() { return counterAccount; }
+//    public void setCounterAccount(Account counterAccount) { this.counterAccount = counterAccount; }
 
     @Override
     public String toString() {
